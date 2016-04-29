212f6b8fcacc8a4a45b9159b02f9a6451399d0aa
Marko Stankovic <stane-sm@hotmail.com>
2016-04-29 09:42:39 +0200

    promenjeni identi prema pep8

--- klase/login.py
+++ klase/login.py
@@ -1,30 +1,29 @@
 class Login(object):
 
-	def __init__(self,username,password):
-		self.ID, self.username, self.imePrezime, self.uloga = self.__checkCreds(username,password)
+    def __init__(self,username,password):
+        self.ID, self.username, self.imePrezime, self.uloga = self.__checkCreds(username,password)
 
-	def __readFile(self):
-		fileName = "files/korisnici.txt"
-		f = open(fileName,"r")
-		lines = f.readlines()
-		f.close()
-		return lines
+    def __readFile(self):
+        fileName = "files/korisnici.txt"
+        f = open(fileName,"r")
+        lines = f.readlines()
+        f.close()
+        return lines
 
+    def __checkCreds(self,username,password):
+        lines = self.__readFile()
+        for line in lines:
+            l = line.strip().split("|")
+            ID = l[0]
+            imePrezime = l[1]
+            uloga = l[4]
 
-	def __checkCreds(self,username,password):
-		lines = self.__readFile()
-		for line in lines:
-			l = line.strip().split("|")
-			ID = l[0]
-			imePrezime = l[1]
-			uloga = l[4]
+            if username == l[2] and password == l[3]:
+                return ID, username, imePrezime, uloga
+            else:
+                x = 1
 
-			if username == l[2] and password == l[3]:
-				return ID, username, imePrezime, uloga
-			else:
-				x = 1
-
-		if x != 0:
-			return None,None,None,None
+        if x != 0:
+            return None,None,None,None
 
 
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from klase.login import *
-from klase.korisnik import *
 from GUI.windows import *
+
 
 def main():
 
-	application = Glavna()
-	application.mainloop()
+    application = Glavna()
+    application.mainloop()
 
 if __name__ == "__main__":
-	main()
+    main()
