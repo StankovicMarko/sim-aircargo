2f1eecc03c91f30e2735c627355bf6f7c67293cc
Marko Jokic <markojokic1996@gmail.com>
2016-04-30 16:18:51 -0400

    Inicijalizacija klaze ZahtevZaSmestanjeAviona i mala izmena

--- klase/zahtevi.py
+++ klase/zahtevi.py
@@ -1,10 +1,26 @@
-class ZahtevZaTransport(object):
-	def __init__(self, ID,datumKreiranja,datumTransporta,odrediste,IDPotrazitelja,oznakaAviona,statusZahteva):
-		self.ID = ID
+class Zahtev(object):
+	def __init__(self, IDZahteva,datumKreiranja,oznakaAviona):
+		super(Zahtev, self).__init__()
+		self.IDZahteva = IDZahteva
 		self.datumKreiranja = datumKreiranja
+		self.oznakaAviona = oznakaAviona
+		
+
+
+class ZahtevZaTransport(Zahtev):
+	def __init__(self, IDZahteva,datumKreiranja,datumTransporta,odrediste,IDPotrazitelja,oznakaAviona,statusZahteva):
+		Zahtev.__init__(self,IDZahteva,datumKreiranja,oznakaAviona)
 		self.datumTransporta = datumTransporta
 		self.odrediste = odrediste
 		self.IDPotrazitelja = IDPotrazitelja
-		self.oznakaAviona = oznakaAviona
 		self.statusZahteva = statusZahteva
+		
+
+class ZahtevZaSmestanjeAviona(Zahtev):
+	def __init__(self,IDZahteva,datumKreiranja,vremeSmestanjaUHangar,vremeNapustanjaHangara,oznakaHangara,oznakaAviona,IDMenadzeraHangara):
+		Zahtev.__init__(self,IDZahteva,datumKreiranja,oznakaAviona)
+		self.vremeSmestanjaUHangar = vremeSmestanjaUHangar
+		self.vremeNapustanjaHangara = vremeNapustanjaHangara
+		self.oznakaHangara = oznakaHangara
+		self.IDMenadzeraHangara = IDMenadzeraHangara
 		
