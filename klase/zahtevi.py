d95e1d805cf9da897db9ea2afa372f76dffaa7e3
Marko Jokic <markojokic1996@gmail.com>
2016-04-30 15:31:49 -0400

    Inicijalizacija klase zahtevZaTransport

---
+++ klase/zahtevi.py
@@ -0,0 +1,10 @@
+class ZahtevZaTransport(object):
+	def __init__(self, ID,datumKreiranja,datumTransporta,odrediste,IDPotrazitelja,oznakaAviona,statusZahteva):
+		self.ID = ID
+		self.datumKreiranja = datumKreiranja
+		self.datumTransporta = datumTransporta
+		self.odrediste = odrediste
+		self.IDPotrazitelja = IDPotrazitelja
+		self.oznakaAviona = oznakaAviona
+		self.statusZahteva = statusZahteva
+		
